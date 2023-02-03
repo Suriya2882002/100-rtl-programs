1ee8869a981e2b8017aad64882daed213593d945
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-02-03 11:29:47 +0530

    Add files via upload

---
+++ adder subtractor/ripple_carry_adder_subtractor.v
@@ -0,0 +1,17 @@
+module ripple_carry_adder_subtractor #(parameter SIZE = 4) (
+  input [SIZE-1:0] A, B, 
+  input CTRL,
+  output [SIZE-1:0] S, Cout);
+  bit [SIZE-1:0] Bc;
+  genvar g;
+  
+  assign Bc[0] = B[0] ^ CTRL;
+    
+  full_adder fa0(A[0], Bc[0], CTRL, S[0], Cout[0]);
+  generate  
+    for(g = 1; g<SIZE;g =g+1) begin
+      assign Bc[g] = B[g] ^ CTRL;
+      full_adder fa(A[g], Bc[g], Cout[g-1], S[g], Cout[g]);
+    end
+  endgenerate
+endmodule
