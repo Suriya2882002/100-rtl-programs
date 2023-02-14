3b59067a9818c765f85657edafa11e93cf3e2a33
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-02-14 10:47:10 +0530

    Delete slow_decade_counter.v

--- slow decade counter/slow_decade_counter.v
+++
@@ -1,7 +0,0 @@
-module slow_down_counter(input clk,input slowena,input reset,output reg[3:0]q);
-  always@(posedge clk)begin
-    if(reset)q<=0;
-    else if(slowena && q==9)q<=0;
-    else if(slowena)q<=q+1;
-    end
-  endmodule
